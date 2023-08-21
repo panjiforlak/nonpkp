9d70794b7eedc1d8a180eaffcc0af1c67e5554c6
panji <panji@bakingworld.id>
2023-08-21 15:56:20 +0700

    update

--- application/modules/template/views/includes/header.php
+++ application/modules/template/views/includes/header.php
@@ -1,6 +1,6 @@
-<a style="position: fixed;height:44px;line-height:44px;background-color:#023b6d" href="<?php echo base_url('home') ?>" class="logo">
+<a style="position: fixed;height:44px;line-height:44px;background-color:#FFA500" href="<?php echo base_url('home') ?>" class="logo">
     <span class="logo-lg">
-        ERP SYSTEMs <sup>v1.2</sup>
+        <b style="color: black;">NON PKP</b> ERP<sup>v1.2</sup>
         <!-- <img style="height: 30px;" src="<?php echo base_url((!empty($setting->logo) ? $setting->logo : 'assets/img/icons/logo.png')) ?>" alt=""> -->
     </span>
     <span class="logo-mini">
@@ -13,7 +13,7 @@
 <?php $gui_p = $this->uri->segment(1);
 if ($gui_p != 'gui_pos') {
 ?>
-    <nav class="navbar " style="position: fixed;right:0;left:0;top:0;height:45px;min-height:45px;background-color:#023b6d">
+    <nav class="navbar " style="position: fixed;right:0;left:0;top:0;height:45px;min-height:45px;background-color:#FFA500">
         <a href="#" class="sidebar-toggle text-muted" data-toggle="offcanvas" role="button" style="border-right:0px;height:45px;margin-top:3px;padding-top:5px"> <!-- Sidebar toggle button-->
             <span class="sr-only">Toggle navigation</span>
             <span class="pe-7s-menu"></span>
@@ -23,20 +23,20 @@ if ($gui_p != 'gui_pos') {
 
             if ($this->permission1->method('new_invoice', 'create')->access()) {
             ?>
-                <a style="margin-top:5px" href="<?php echo base_url('add_invoice') ?>" class="btn btn-sm btn-primary btn-outline"><i class="fa fa-shopping-bag"></i> <?php echo display('invoice') ?></a>
+                <a style="margin-top:5px" href="<?php echo base_url('add_invoice') ?>" class="btn btn-sm btn-black"><i class="fa fa-shopping-bag"></i> <?php echo display('invoice') ?></a>
             <?php } ?>
 
 
             <?php if ($this->permission1->method('customer_receive', 'create')->access()) { ?>
-                <a style="margin-top:5px" href="<?php echo base_url('customer_receive') ?>" class="btn btn-sm btn-primary btn-outline"><i class="fa fa-money"></i> <?php echo display('customer_receive') ?></a>
+                <a style="margin-top:5px" href="<?php echo base_url('customer_receive') ?>" class="btn btn-sm btn-black "><i class="fa fa-money"></i> <?php echo display('customer_receive') ?></a>
             <?php } ?>
 
             <?php if ($this->permission1->method('supplier_payment', 'create')->access()) { ?>
-                <a style="margin-top:5px" href="<?php echo base_url('supplier_payment') ?>" class="btn btn-sm btn-primary btn-outline"><i class="fa fa-money" aria-hidden="true"></i> <?php echo display('supplier_payment') ?></a>
+                <a style="margin-top:5px" href="<?php echo base_url('supplier_payment') ?>" class="btn btn-sm btn-black "><i class="fa fa-money" aria-hidden="true"></i> <?php echo display('supplier_payment') ?></a>
             <?php } ?>
 
             <?php if ($this->permission1->method('add_purchase', 'create')->access()) { ?>
-                <a style="margin-top:5px" href="<?php echo base_url('add_purchase') ?>" class="btn btn-sm btn-primary btn-outline"><i class="ti-shopping-cart"></i> <?php echo display('purchase') ?></a>
+                <a style="margin-top:5px" href="<?php echo base_url('add_purchase') ?>" class="btn btn-sm btn-black "><i class="ti-shopping-cart"></i> <?php echo display('purchase') ?></a>
             <?php } ?>
         </span>
 
@@ -47,7 +47,7 @@ if ($gui_p != 'gui_pos') {
                 <?php if ($this->permission1->method('pos_invoice', 'create')->access()) {
                 ?>
                     <li>
-                        <a style="margin-top:5px" href="<?php echo base_url('gui_pos') ?>" class="text-white btn-sm btn-success pos-btn"> <span class="fa fa-plus"></span> <?php echo display('pos_invoice') ?></a>
+                        <a style="margin-top:5px" href="<?php echo base_url('gui_pos') ?>" class="text-white btn-sm btn-primary pos-btn"> <span class="fa fa-plus"></span> <?php echo display('pos_invoice') ?></a>
                     </li>
                 <?php } ?>
                 <?php if ($max_version > $current_version) { ?>
